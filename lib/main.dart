de3cf9a36a76ffd161753eb7a72c89da668568de
gjmfs <mufeesmc28@gmail.com>
2025-02-17 23:12:05 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MainAppState();
+  }
+}
+
+class MainAppState extends State<MainApp> {
+  List<String> task = [];
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController inputBox1 = TextEditingController();
+    return (MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 82, 28, 25),
+          title: Text("To-do App", style: TextStyle(color: Colors.white)),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: inputBox1,
+                decoration: InputDecoration(
+                  label: Text("Enter a task"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            MaterialButton(
+              color: Colors.white,
+              height: 50,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (!inputBox1.text.isEmpty) {
+                    task.add(inputBox1.text);
+                  }
+                });
+              },
+              child: Text("+"),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: task.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(task[index]),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: MaterialButton(
+                            color: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                task.removeAt(index);
+                              });
+                            },
+
+                            child: Text(
+                              "-",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 224, 224, 224),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
